e1e7a0e56d6330a1b05f67506a549de49cdf818b
diarragreene <tsunokad@mcmaster.ca>
2025-07-06 01:58:11 +0900

    Add jobCardDetailsComponent for Page1

---
+++ src/components/jobsCardComponent.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  jobName?: string
+  organizationName?: string
+  gradeLevelText?: string
+  schoolBoardText?: string
+}>()
+</script>
+
+<template>
+  <div class="card">
+    <div
+      class="card-header camp-gradient h-24 rounded-t-md bg-gradient-to-b from-purple-300 to-purple-800"
+    ></div>
+    <div class="card-content mt-1">
+      <div class="flex justify-between items-center mb-0.5">
+        <h3 class="text-lg font-semibold">
+          {{ jobName || 'Job Name Goes Here' }}
+        </h3>
+        <span class="badge grade-level flex items-center">{{
+          gradeLevelText || 'Grade Level'
+        }}</span>
+      </div>
+
+      <div class="card-footer flex items-center justify-between">
+        <span class="organization-name text-sm text-gray-700">
+          {{ organizationName || 'Organization Name' }}
+        </span>
+        <span class="badge school-board">{{ schoolBoardText || 'School Board' }}</span>
+      </div>
+    </div>
+  </div>
+</template>
