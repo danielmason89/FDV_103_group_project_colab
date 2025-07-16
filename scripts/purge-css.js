b4fdc3dcd2f73f34960a3b3777a3861b3165feef
Daniel Mason <danmasson0@gmail.com>
2025-07-16 01:58:59 -0400

    Add purge-css GitHub Action part #3

--- scripts/purge-css.js
+++ scripts/purge-css.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const TARGET_DIR = path.resolve(__dirname, '../css');
+const TARGET_DIR = path.resolve(__dirname, '../src/assets');
 
 function purgeCssFiles(dir) {
   fs.readdirSync(dir).forEach((file) => {
