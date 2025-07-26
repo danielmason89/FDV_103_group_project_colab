52a3ee4577339181b55bc91e499be40978dd5316
Natalie Ivanis <eilatansinavi@gmail.com>
2025-07-25 21:35:03 -0400

    VeeValidate cleanup - Form CheckboxGrid&RadioGroup components.

--- src/components/FormRadioGroup.vue
+++ src/components/FormRadioGroup.vue
@@ -1,41 +1,43 @@
 <script setup lang="ts">
+import { useField } from 'vee-validate'
+
 interface RadioOption {
   value: string
   label: string
 }
-
-interface Props {
+const props = defineProps<{
+  name: string
   label: string
   options: RadioOption[]
   modelValue: string
   required?: boolean
-  erro?: string
-}
+}>()
 
-interface Emits {
-  (e: 'update:modelValue', value: string): void
-}
-
-defineProps<Props>()
-defineEmits<Emits>()
+const { value, errorMessage, setValue, handleBlur } = useField<string>(props.name)
 </script>
 
 <template>
   <div class="form-group">
     <label class="form-label"> {{ label }}{{ required ? '*' : '' }} </label>
-    <div class="radio-group" :class="['border rounded px-3 py-2', error && 'field-border-error']">
-      <label v-for="option in options" :key="option.value" class="radio-item">
+    <div
+      class="radio-group"
+      :class="['border rounded px-3 py-2', errorMessage && 'field-border-error']"
+    >
+      <label v-for="opt in options" :key="opt.value" class="radio-item">
         <input
-          type="radio"
-          :value="option.value"
-          :checked="modelValue === option.value"
-          @change="$emit('update:modelValue', option.value)"
           class="form-radio"
+          type="radio"
+          :value="opt.value"
+          :checked="value === opt.value"
+          @change="() => setValue(opt.value)"
+          @blur="handleBlur"
         />
-        <span>{{ option.label }}</span>
+        <span>{{ opt.label }}</span>
       </label>
     </div>
-    <span v-if="error" class="field-text-error">{{ error }}</span>
+    <span v-if="errorMessage" class="field-text-error text-red-600 text-sm">{{
+      errorMessage
+    }}</span>
   </div>
 </template>
 
